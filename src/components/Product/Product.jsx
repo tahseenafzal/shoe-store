b46dc8c18c6e5ebb7a65b08d6f438a53ceb477d7
Tehseen Afzal <abuashja@gmail.com>
2021-08-24 23:49:36 +0500

    Update Product.jsx
    Add grid component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -16,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function FullWidthGrid() {
+export default function Product() {
   const classes = useStyles();
 
   return (
