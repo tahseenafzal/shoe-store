da7d3cada4760cbe9cc279a7dca8a516c60e46b1
Tehseen Afzal <abuashja@gmail.com>
2021-08-25 19:32:04 +0500

    Update index.js
    export Navbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Product } from "./Product/Product";
 export { default as NotFound } from "./Product/NotFound";
 export { default as ProductItem } from "./Product/ProductItem";
+export { default as NavBar } from "./NavBar";
