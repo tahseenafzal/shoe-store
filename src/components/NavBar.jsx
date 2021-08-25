bda2e03898f36750e3903eaf4a70763f5df0d390
Tehseen Afzal <abuashja@gmail.com>
2021-08-25 19:32:10 +0500

    Create NavBar.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: "white",
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4" className={classes.title}>
+            The Shoe Store
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
